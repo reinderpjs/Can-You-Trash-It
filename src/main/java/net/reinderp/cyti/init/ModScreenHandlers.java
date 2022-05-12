d4f8bfa9c45c378db1782bc1c963304362bc79ea
reinderp <57286567+reinderpjs@users.noreply.github.com>
2022-05-12 20:08:15 +0200

    Update ModScreenHandlers.java

--- src/main/java/net/reinderp/cyti/init/ModScreenHandlers.java
+++ src/main/java/net/reinderp/cyti/init/ModScreenHandlers.java
@@ -1,8 +1,12 @@
 package net.reinderp.cyti.init;
 
 import net.fabricmc.fabric.api.client.screenhandler.v1.ScreenRegistry;
+import net.fabricmc.fabric.api.screenhandler.v1.ExtendedScreenHandlerType;
 import net.fabricmc.fabric.api.screenhandler.v1.ScreenHandlerRegistry;
+import net.minecraft.client.gui.screen.ingame.HandledScreen;
+import net.minecraft.client.gui.screen.ingame.HandledScreens;
 import net.minecraft.screen.ScreenHandlerType;
+import net.minecraft.util.registry.Registry;
 import net.reinderp.cyti.render.guis.trashcan_energy.EnergyTrashcanScreen;
 import net.reinderp.cyti.render.guis.trashcan_energy.EnergyTrashcanScreenHandler;
 import net.reinderp.cyti.render.guis.trashcan_fluid.FluidTrashCanScreenHandler;
@@ -18,17 +22,17 @@ public class ModScreenHandlers {
     public static final ScreenHandlerType<EnergyTrashcanScreenHandler> ENERGY_TRASHCAN_SCREEN_HANDLER;
 
     static {
-        ITEM_TRASHCAN_SCREEN_HANDLER = ScreenHandlerRegistry.registerSimple(Identifiers.TRASHCAN_ITEM,
-                ItemTrashcanScreenHandler::new);
-        FLUID_TRASHCAN_SCREEN_HANDLER = ScreenHandlerRegistry.registerSimple(Identifiers.TRASHCAN_FLUID,
-                FluidTrashCanScreenHandler::new);
-        ENERGY_TRASHCAN_SCREEN_HANDLER = ScreenHandlerRegistry.registerSimple(Identifiers.TRASHCAN_ENERGY,
-                EnergyTrashcanScreenHandler::new);
+        ITEM_TRASHCAN_SCREEN_HANDLER = Registry.register(Registry.SCREEN_HANDLER, Identifiers.TRASHCAN_ITEM,
+                new ScreenHandlerType<>(ItemTrashcanScreenHandler::new));
+        FLUID_TRASHCAN_SCREEN_HANDLER =  Registry.register(Registry.SCREEN_HANDLER, Identifiers.TRASHCAN_FLUID,
+                new ScreenHandlerType<>(FluidTrashCanScreenHandler::new));
+        ENERGY_TRASHCAN_SCREEN_HANDLER =  Registry.register(Registry.SCREEN_HANDLER, Identifiers.TRASHCAN_ENERGY,
+                new ScreenHandlerType<>(EnergyTrashcanScreenHandler::new));
     }
 
     public static void ClientInitialize() {
-        ScreenRegistry.register(ITEM_TRASHCAN_SCREEN_HANDLER, ItemTrashcanScreen::new);
-        ScreenRegistry.register(FLUID_TRASHCAN_SCREEN_HANDLER, FluidTrashcanScreen::new);
-        ScreenRegistry.register(ENERGY_TRASHCAN_SCREEN_HANDLER, EnergyTrashcanScreen::new);
+        HandledScreens.register(ITEM_TRASHCAN_SCREEN_HANDLER, ItemTrashcanScreen::new);
+        HandledScreens.register(FLUID_TRASHCAN_SCREEN_HANDLER, FluidTrashcanScreen::new);
+        HandledScreens.register(ENERGY_TRASHCAN_SCREEN_HANDLER, EnergyTrashcanScreen::new);
     }
 }
